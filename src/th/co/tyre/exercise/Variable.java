51a56602fc7aa3f058d876848a97377fa5e7d97d
kritbovorn <ktaweeyossak@yahoo.com>
2021-03-01 21:44:59 +0700

    Finish 2.1

--- src/th/co/tyre/exercise/Variable.java
+++ src/th/co/tyre/exercise/Variable.java
@@ -1,5 +1,34 @@
 package th.co.tyre.exercise;
 
 public class Variable {
+	
+	// 1 comment
+	
+//	String singleLine = "This is comment Single Line";
+	
+	/* String MultipleLineString = "This is comment Multiple Line";
+	 MultipleLineNumber = 11;   */
+	
+	
+	
+	public static void main(String[] args) {
+		int num = 7;
+		boolean isTrue = false;
+		String myString = "My String";
+		float floatNum = 0.7f;
+		double doubleNum = 0.9d;
+		char charactor = '\u0041';
+		
+		
+		System.out.println("This is a Number : " + num);
+		System.out.println("This is a Boolean : " + isTrue);
+		System.out.println("This is a String : " + myString);
+		System.out.println("This is a Float num : " + floatNum);
+		System.out.println("This is a Double num : " + doubleNum);
+		System.out.println("This is a Charactor : " + charactor);
+	}
+	
+	
+	
 
 }
